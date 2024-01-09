9ffa9a9b905aa7c3e49220de00c7c9cbf7cb4794
rodrigo.rosa02 <rodrigo.rosa81@edu.pucrs.br>
2024-01-08 22:39:28 -0300

    feat: route "listAllUsers" created.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,31 +1,46 @@
 import { ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode, } from 'http-status-codes';
 import { userData } from '../validations/userValidation';
 import * as userService from "../services/userService";
+import { Request, Response } from "express";
 
-export const createUser = async (userData: type) => {
-
-};
-
-export const updateUser = async (userData: type) => {
+export const createUser = async (userData: any) => {
 
 };
 
-export const listAllUsers = async (userData: type) => {
+export const updateUser = async (userData: any) => {
 
 };
 
-export const removeUser = async (userData: type) => {
+export const listAllUsers = async (req: Request, res: Response) => {
+
+  try {
+    const {
+      userid, usertype, gender, createdat, updatedat
+    } = req.query as Record<string, any>;
+
+    const result = await userService.listAllUsers({
+      userid, usertype, gender, createdat, updatedat
+    });
+
+    res.json(result);
+
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const removeUser = async (userData: any) => {
 
 };
 
-export const getUserById = async (userData: type) => {
+export const getUserById = async (userData: any) => {
 
 };
 
-export const getUserByEmail = async (userData: type) => {
+export const getUserByEmail = async (userData: any) => {
 
 };
 
-export const getUserNamebyEmail = async (userData: type) => {
+export const getUserNamebyEmail = async (userData: any) => {
 
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,30 +1,35 @@
 import { errorMessages, successMessages } from "../messages/messages";
 import * as userRepository from "../repositories/userRepository";
 
-export const createUser = async (userData: type) => {
+export const createUser = async (userData: any) => {
 
 };
 
-export const updateUser = async (userData: type) => {
+export const updateUser = async (userData: any) => {
 
 };
 
-export const listAllUsers = async (userData: type) => {
+export const listAllUsers = async (queryParams: any) => {
+  const result = await userRepository.listAllUsers(queryParams);
+  if (result.length === 0) {
+    throw new Error(errorMessages.USERS_NOT_FOUND);
+  }
+  return result;
+  // return needs to be a DTO format for specific presentation of results.
+};
+
+export const removeUser = async (userData: any) => {
 
 };
 
-export const removeUser = async (userData: type) => {
+export const getUserById = async (userData: any) => {
 
 };
 
-export const getUserById = async (userData: type) => {
+export const getUserByEmail = async (userData: any) => {
 
 };
 
-export const getUserByEmail = async (userData: type) => {
-
-};
-
-export const getUserNamebyEmail = async (userData: type) => {
+export const getUserNamebyEmail = async (userData: any) => {
 
 }
